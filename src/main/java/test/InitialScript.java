082d226fcd41803e831519f0f17c29779b96adfa
Sudhanshu <subansal@iitk.ac.in>
2020-06-05 03:28:20 +0530

    Added Initial Script to gradle

--- src/main/java/test/InitialScript.java
+++ src/main/java/test/InitialScript.java
@@ -8,15 +8,15 @@ import research.Response;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
-public class test {
+public class InitialScript {
     public static void main(String[] args) throws IOException, URISyntaxException {
         Redis myObject = Redis.getInstance();
         FakeJsonServer servers = FakeJsonServer.getInstance(10);
-        MyHttpServer server = servers.getServer("localhost",8002);
+        MyHttpServer server = servers.getServer("localhost",8001);
 //        MyHttpServer server2 = servers.getServer("localhost",8002);
 //
-        server.newResponse("/test", new Response().setGetResponse("{\"method\" : \"get\"}").setPostResponse("{\"method\" : \"post\"}"))
-                .newResponse("/news/worlde",new Response().setGetResponse("{\"method\" : \"get\"}").setPostResponse("{\"method\" : \"post\"}"));
+        server.newResponse("/test/", new Response().setGetResponse("{\"method\" : \"get\"}").setPostResponse("{\"method\" : \"post\"}"))
+                .newResponse("/news/worlde/",new Response().setGetResponse("{\"method\" : \"get\"}").setPostResponse("{\"method\" : \"post\"}"));
 //
 //        server.newResponse("/testUpdate",server.getResponse("/test").setGetResponse("{\"method\" : \"getUpdate\"}"));
 //        server.forward("/news", "http://localhost:8002/");
